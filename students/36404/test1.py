e99c5da5d7a91f8fd5cf8915a9d83f0b7ac652dc
aziretych <aziretpochta@gmail.com>
2026-03-29 19:36:31 +0000

    Wrote code for test1 task

--- students/36404/test1.py
+++ students/36404/test1.py
@@ -1,1 +1,12 @@
+# Variables
+name = "Alice"
+age = 25
+score = 95.5
 
+# Lists
+fruits = ["apple", "banana", "orange", "grape"]
+numbers = [10, 20, 30, 40, 50]
+
+# Loops
+for fruit in fruits:
+    print(fruit)
