53019c6aae22cae60d6f58b490d18c8ce9216522
aziretych <aziretpochta@gmail.com>
2026-03-29 19:49:10 +0000

    Wrote code for the second lab task

---
+++ students/36404/lecture_2_entry_test.py
@@ -0,0 +1,17 @@
+# My birthday
+my_bday = 1
+
+print(my_bday)
+print(type(my_bday))
+
+# My name
+my_name = "Aziret Kubatbekov"
+
+print(my_name)
+print(type(my_name))
+
+# My grade
+my_grade = 3.89
+my_grade = int(my_grade)
+print(my_grade)
+print(type(my_grade))
